2bba19a1d6fa0af39c415a8f02b6b653100d2ef6
dadddda <daduka9k@gmail.com>
2021-04-29 22:33:27 +0400

    Minor change to webpack config output path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "public/build"),
     filename: "main.js"
   },
   target: "node",
