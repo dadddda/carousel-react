18ee562b920f3139139d290d563d6e861b1186ba
dadddda <daduka9k@gmail.com>
2021-04-29 22:27:03 +0400

    Initial upload of webpack and babel config files

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js"
+  },
+  target: "node",
+  devServer: {
+    port: "8080",
+    contentBase: ["./public"],
+    open: true
+  },
+  resolve: {
+    extensions: [".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+}
