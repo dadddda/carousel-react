57c59dc8a88724cc65cb3917539c7bc8dcf5f8d4
dadddda <daduka9k@gmail.com>
2021-05-02 23:49:22 +0400

    For code refactoring created constants.js and utils.js

---
+++ src/helpers/constants.js
@@ -0,0 +1,39 @@
+const slides = [
+  {
+    id: 0,
+    title: "Slide 0"
+  },
+  {
+    id: 1,
+    title: "Slide 1"
+  },
+  {
+    id: 2,
+    title: "Slide 2"
+  },
+  {
+    id: 3,
+    title: "Slide 3"
+  },
+  {
+    id: 4,
+    title: "Slide 4"
+  },
+  {
+    id: 5,
+    title: "Slide 5"
+  }
+]
+
+const singleView = "single";
+const multipleView = "multiple";
+const animated = "animated";
+const jumpThreshold = 100;
+
+export {
+  slides,
+  singleView,
+  multipleView,
+  animated,
+  jumpThreshold
+};
---
+++ src/helpers/utils.js
@@ -0,0 +1,69 @@
+import * as K from "./constants";
+
+// given view component, it's slides component and swipe length,
+// calculates new x(left) position for slides and assigns that new
+// value to slides component
+const repositionSlides = (activeComp, slidesComp, actionXDiff) => {
+  let activeCompBr = activeComp.getBoundingClientRect();
+  let slidesCompBr = slidesComp.getBoundingClientRect();
+
+  let slidesXPos = slidesCompBr.left - activeCompBr.left;
+  let newSlidesXPos = slidesXPos - actionXDiff;
+
+  let slidesStartX = 0;
+  let slidesEndX = slidesCompBr.width - activeCompBr.width;
+  if (newSlidesXPos > 0) newSlidesXPos = slidesStartX;
+  else if (newSlidesXPos < slidesEndX * -1) newSlidesXPos = slidesEndX * -1;
+
+  slidesComp.style.left = newSlidesXPos + "px";
+}
+
+// given slides component and slide number(0...n), repositions
+// slide with given slide number in the leftmost part of the viewport
+// with animation
+const jumpToSlide = (slidesComp, slideNum) => {
+  let slidesCount = slidesComp.children.length;
+  let slideWidth = slidesComp.offsetWidth / slidesCount;
+
+  slidesComp.classList.add(K.animated);
+  
+  let newSlidesXPos = slideNum * slideWidth * -1;
+  slidesComp.style.left = newSlidesXPos + "px";
+
+  setTimeout(() => {
+    slidesComp.classList.remove(K.animated);
+  }, 200);
+}
+
+// according to given swipe start coordinate, end coordinate, current
+// slide number and total number of slides, returns new value of slide
+// number(next/previous) which must be displayed
+const adjCurrSlide = (startX, endX, currSlide, slidesCount) => {
+  let newSlide = currSlide;
+
+  if (Math.abs(startX - endX) > K.jumpThreshold) {
+    if (startX > endX && currSlide < slidesCount - 1) {
+      newSlide++;
+    } else if (startX < endX && currSlide > 0) {
+      newSlide--;
+    }
+  }
+
+  return newSlide;
+}
+
+// returns client x coordinate of touch/mouse action
+const getActionX = (e) => {
+  let coordinateData = e;
+  let touch = e.touches;
+  if (touch !== undefined) coordinateData = touch[0];
+
+  return coordinateData.clientX;
+}
+
+export {
+  repositionSlides, 
+  jumpToSlide, 
+  adjCurrSlide,
+  getActionX
+};
